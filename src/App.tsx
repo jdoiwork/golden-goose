a8279a3154a81fe998b96d38a0f9c026b6a56f1d
Junichiro Doi <jdoi.work@gmail.com>
2022-04-24 12:09:04 +0000

    Add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import React from 'react';
 
 import './App.css';
 
+type State = {
+  api_key: string
+  secret_key: string
+  binance?: unknown
+}
+
 function App() {
+  const [state, setState] = React.useState<State>({
+    api_key: "",
+    secret_key: "",
+    binance: null,
+  })
+
+  const handleCreate = () => {
+    console.log("create")
+
+    setState(s => ({
+      ...s,
+      
+    }))
+  }
+
+  const handleChange = (key: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState(s => ({
+      ...s,
+      [key]: event.target.value,
+    }))
+  }
+
   return (
     <div className="App">
-      <Button variant="contained">Contained</Button>
+      <TextField id="api-key" label="API Key" variant="outlined" value={state.api_key} onChange={handleChange("api_key")} />
+      <TextField id="secret-key" label="Secret Key" variant="outlined" value={state.secret_key} onChange={handleChange("secret_key")}/>
+      <Button variant="contained" onClick={handleCreate}>Create</Button>
     </div>
   );
 }
